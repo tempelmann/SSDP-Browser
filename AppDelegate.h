9b0e7c449e42076dac1966f919fd173259aa4027
Thomas Tempelmann <tempelmann@gmail.com>
2024-03-07 01:55:42 +0100

    App now quits when last window is closed.

--- AppDelegate.h
+++ AppDelegate.h
@@ -8,7 +8,4 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-
-
 @end
-
--- AppDelegate.m
+++ AppDelegate.m
@@ -8,19 +8,12 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+	return YES;
 }
-
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-	// Insert code here to tear down your application
-}
-
 
 @end
